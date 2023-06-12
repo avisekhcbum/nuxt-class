6369125466fdaefc70ae3296be64a598b76b0c79
ujwal limbugit config --global user.name ujwal <saritatamrakar092@gmail.com>
2023-06-12 02:34:25 -0700

    product made

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,60 @@
 <template>
-    <div>
-<section class="hero py-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>this is home page</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos pariatur voluptas repudiandae eligendi fugiat magni ea explicabo laudantium a natus quasi, iusto sint minima aspernatur.</p>
+<div>
+    <section class="hero py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>this is home page</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos pariatur voluptas repudiandae eligendi fugiat magni ea explicabo laudantium a natus quasi, iusto sint minima aspernatur.</p>
+                </div>
             </div>
+           
+
         </div>
-        <div>
-            <section>
 
-                <!-- card -->
-                <section class="py-5">
-                    <div class="container">
-                        <div class="row">
-                            <div col-md-4>
-                                <div class="card">
-                                    <img src="https://cdn.pixabay.com/photo/2023/05/31/12/20/car-8031261__340.jpg" alt="">
-                                    <h1>this is a picture of a car</h1>
+    </section>
+    
+                    <!-- card -->
+                    <section class="py-5">
+                        <div class="container">
+                            <div class="row">
+                                <div col-md-4>
+                                    <div class="card">
+                                        <img src="https://cdn.pixabay.com/photo/2023/05/31/12/20/car-8031261__340.jpg" alt="">
+                                        <h1>this is a picture of a car</h1>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="col-md-8">
-                                <div class="card">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <img src="https://cdn.pixabay.com/photo/2023/05/29/20/55/mountains-8027230_640.jpg" alt="">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ratione cumque dolorum dignissimos deserunt qui deleniti tempora, quaerat autem molestiae nihil debitis ea accusamus explicabo possimus similique doloribus, sunt delectus!</p>
+                                <div class="col-md-8">
+                                    <div class="card">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <img src="https://cdn.pixabay.com/photo/2023/05/29/20/55/mountains-8027230_640.jpg" alt="">
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia ratione cumque dolorum dignissimos deserunt qui deleniti tempora, quaerat autem molestiae nihil debitis ea accusamus explicabo possimus similique doloribus, sunt delectus!</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </section>
-            </section>
-
-        </div>
-
-    </div>
-    
-</section>
-    </div>
+                    </section>
+</div>
 </template>
 
 <script setup>
 
 </script>
 
-<style lang="scss" scoped>
-.hero{
+<style  scoped>
+.hero {
     background-color: blue;
     text-align: center;
     height: 400px;
 }
-.card{
+
+.card {
     border-radius: 10px;
     overflow: hidden;
     box-shadow: 10px 10px 10px 10px goldenrod;
---
+++ pages/products/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <section class="py-5">
+        <div class="container">
+            <div class="row g-3">
+                <div class="col-md-3" v-for="(product,index) in 30" :key="index">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://cdn.pixabay.com/photo/2023/03/02/20/59/town-hall-tower-7826491__340.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+</template>

2e17ef87e0d00ace5d2117879557c3cd87f6ae42
ujwal limbugit config --global user.name ujwal <saritatamrakar092@gmail.com>
2023-06-13 15:06:48 +0545

    id added

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <h1>Product Detailed Page</h1>
+    <h2>The Product Number is {{ route.params.id }}</h2>
+</div>
+</template>
+
+<script>
+const route = useRoute();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -5,7 +5,9 @@
             <div class="row g-3">
                 <div class="col-md-3" v-for="(product,index) in 30" :key="index">
                     <div class="card" style="width: 18rem;">
-                        <img src="https://cdn.pixabay.com/photo/2023/03/02/20/59/town-hall-tower-7826491__340.jpg" class="card-img-top" alt="...">
+                        <div class="image-box">
+                            <img src="https://cdn.pixabay.com/photo/2023/03/02/20/59/town-hall-tower-7826491__340.jpg" class="card-img-top" alt="...">
+                        </div>
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -18,3 +20,22 @@
     </section>
 </div>
 </template>
+
+
+<script setup>
+
+</script>
+
+<style  scoped>
+img{
+    filter: grayscale();
+    transition: 1s;
+}
+img:hover{
+    filter: none;
+    transform: scale(2);
+}
+.image-box{
+    overflow: hidden;
+}
+</style>

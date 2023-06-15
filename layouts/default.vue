015cba39329f4153830031846566dd9a1b8e36c7
ujwal limbugit config --global user.name ujwal <saritatamrakar092@gmail.com>
2023-06-15 15:06:32 +0545

    code it edit

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
         <header>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+  <div class="container">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -40,7 +40,40 @@
         </main>
 
         <footer>
-            
+            <section>
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-3">
+                    <img src="https://codeit.com.np/images/1642171339codeIT%20logo.png" alt="" height="60px">
+                    <p>9862130505, 025-525163</p>
+                    <p>info@codeit.com.np</p>
+                    <p>prithivi path, dharan</p>
+                  </div>
+                  <div class="col-md-3">
+                    <h1>Quick links</h1>
+                    <p>About us</p>
+                    <p>Contact link</p>
+                    <p>Gallery</p>
+                    <p>Blog</p>
+                  </div>
+                  <div class="col-md-3">
+                    <h1>Bank Account Detail:</h1>
+                    <p><b>Bank:</b>Nepal Bank Limited</p>
+                    <p><b>Account Name:</b>CODE IT</p>
+                    <p><b>Ac/No.:</b>01600106885462000001</p>
+                    <p><b>branch:</b>Dharan</p>
+                    <img src="https://codeit.com.np/asset/img/fonepay-bg.png" alt="" height="30px">
+                    <img src="https://codeit.com.np/asset/img/codeit-qr.jpg" alt="" height="60px">
+                  </div>
+                  <div class="col-md-3">
+                    <h1>Download App</h1>
+                    <p>Download our mobile app</p>
+                    <img src="https://codeit.com.np/mobile/appstore.png" alt="" height="40px">
+                    <img src="https://codeit.com.np/mobile/googleplay.png" alt="" height="40px">
+                  </div>
+                </div>
+              </div>
+            </section>
         </footer>
     </div>
 </template>
